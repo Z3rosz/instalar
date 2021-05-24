73dd7940660368b08d35a95492676d51d94651fa
Z3rosz <84684796+Z3rosz@users.noreply.github.com>
2021-05-24 03:18:16 -0300

    Add files via upload

---
+++ instalar.sh
@@ -0,0 +1,6 @@
+apt update ; apt upgrade ;
+pkg install python ; apt install nodejs ;
+pkg install zip ; pkg install unzip ;
+apt install git ; cd ; git clone https://github.com/Z3rosz/botbyz3ro.git ; 
+cd botbyz3ro ; unzip z3robot-insta.zip ;
+cd ; cp /instalar/iniciar.sh . ; cd ; sh iniciar.sh
