5ea4feef21eeab5d9d1658229dfd6561cccc85ae
Z3rosz <84684796+Z3rosz@users.noreply.github.com>
2021-05-24 15:10:34 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -3,4 +3,4 @@ pkg install python ; apt install nodejs ;
 pkg install zip ; pkg install unzip ;
 apt install git ; cd ; git clone https://github.com/Z3rosz/botbyz3ro.git ; 
 cd botbyz3ro ; unzip z3robot-insta.zip ;
-cd ; cd instalar ;  sh  iniciar.sh
+cd ~ / instalar ;  sh  iniciar.sh
